249d05d4fcb6efb6588b1331d04527ec3745acce
unknown <talanvor_divine@yahoo.com>
2024-03-29 17:07:42 -0500

    Added functions to script.js

--- assets/script.js
+++ assets/script.js
@@ -25,17 +25,3 @@ function selectTopics(){
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 selectTopics();
-/*
-const topic = "HTML";
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
